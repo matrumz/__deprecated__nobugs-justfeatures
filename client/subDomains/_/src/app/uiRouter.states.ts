9ca989c10402acb9d733de45785808fb0c491a67
matrumz <matrumz@hotmail.com>
2018-06-02 22:44:21 -0400

    States for radial menu samples

--- client/subDomains/_/src/app/uiRouter.states.ts
+++ client/subDomains/_/src/app/uiRouter.states.ts
@@ -1,17 +1,37 @@
 import { NgModule } from "@angular/core";
 import { AppComponent} from "./app.component"
-import { Page1Component } from "./test/page1/page1.component";
-import * as uirouter from "@uirouter/angular";
+import { Ng2StateDeclaration } from "@uirouter/angular";
+import { CircularMenuComponent } from "./samples/menus/radial/circular-menu/circular-menu.component";
+import { Ng2FanMenuComponent } from "./samples/menus/radial/ng2-fan-menu/ng2-fan-menu.component";
+import { OMenuComponent } from "./samples/menus/radial/o-menu/o-menu.component";
+import { WheelnavComponent } from "./samples/menus/radial/wheelnav/wheelnav.component";
 
-export let States: uirouter.Ng2StateDeclaration[] =  [
+
+
+export let States: Ng2StateDeclaration[] =  [
     {
         name: "app",
         url: "/",
         component: AppComponent
     },
     {
-        name: "page1",
-        url: "/test",
-        component: Page1Component
+        name: "circularMenu",
+        url: "/samplemenu/circularmenu",
+        component: CircularMenuComponent
+    },
+    {
+        name: "ng2FanMenu",
+        url: "/samplemenu/fanmenu",
+        component: Ng2FanMenuComponent
+    },
+    {
+        name: "oMenu",
+        url: "/samplemenu/omenu",
+        component: OMenuComponent
+    },
+    {
+        name: "wheelnav",
+        url: "/samplemenu/wheelnav",
+        component: WheelnavComponent
     }
 ]
