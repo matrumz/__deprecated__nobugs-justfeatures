129bd0dd328e2328b81bfef605464c40a9463021
matrumz <matrumz@hotmail.com>
2018-07-07 08:13:08 -0400

    subdomain redirect to localhost working

---
+++ client/site/_dev/fiddlerSiteRedirect/fiddler.rules.cs
@@ -0,0 +1,20 @@
+// static function OnBeforeRequest(oSession: Session) {
+		if (oSession.HostnameIs("mirror.nobugs-justfeatures.com")) {
+			// Handle CONNECT Tunnels
+			if (oSession.HTTPMethodIs("CONNECT"))
+			{
+				oSession["x-replywithtunnel"] = "FakeTunnel";
+				return;
+			}
+			oSession.fullUrl = "http://mirror.localhost:80" + oSession.PathAndQuery;
+		}
+
+		else if (oSession.HostnameIs("www.nobugs-justfeatures.com")) {
+			// Handle CONNECT Tunnels
+			if (oSession.HTTPMethodIs("CONNECT"))
+			{
+				oSession["x-replywithtunnel"] = "FakeTunnel";
+				return;
+			}
+			oSession.fullUrl = "http://www.localhost:80" + oSession.PathAndQuery;
+		}
